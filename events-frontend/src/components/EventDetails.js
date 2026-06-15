d9af812a00c3b05232a718c151d9dd10c80991ec
varun4705 <varun19134@gmail.com>
2026-06-16 03:15:34 +0530

    API Base for localhost is addressed

--- events-frontend/src/components/EventDetails.js
+++ events-frontend/src/components/EventDetails.js
@@ -4,7 +4,7 @@ import { useParams, Link } from 'react-router-dom';
 // const API_BASE_URL = 'http://127.0.0.1:5000/events'; 
 // const API_BASE_URL = 'https://department-management-system-haly.onrender.com/events'; 
 
-const API_URL =
+const API_BASE_URL =
   window.location.hostname === "localhost" ||
   window.location.hostname === "127.0.0.1"
     ? "http://127.0.0.1:5000/events"
